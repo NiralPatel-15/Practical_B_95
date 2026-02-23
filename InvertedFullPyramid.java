51cc4e6302fc65dfafc7296219513a7708e3551d
Niral Patel <niralpatel1502@gmail.com>
2026-02-23 14:39:22 +0530

    Added practical-3 Set B programs

--- InvertedFullPyramid.java
+++ InvertedFullPyramid.java
@@ -1,12 +1,21 @@
 public class InvertedFullPyramid {
+
     public static void main(String[] args) {
 
-        for (int i = 5; i >= 1; i--) {
-            for (int j = 1; j <= i; j++) {
+        int n = 5;
+
+        for (int i = n; i >= 1; i--) {
+
+            for (int space = 1; space <= n - i; space++) {
+                System.out.print(" ");
+            }
+
+
+            for (int star = 1; star <= i; star++) {
                 System.out.print("* ");
             }
+
             System.out.println();
         }
-
     }
 }
